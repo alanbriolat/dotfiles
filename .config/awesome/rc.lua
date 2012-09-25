575fc55377e45c718ba1e5fb81eab47eda635ef0
Alan Briolat <alan.briolat@gmail.com>
2012-09-25 16:21:08 +0100

    Add ctrl+alt+del keybinding for locking xscreensaver

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -227,6 +227,7 @@ globalkeys = awful.util.table.join(
     awful.key({ modkey,           }, "Return", function () awful.util.spawn(terminal) end),
     awful.key({ modkey, "Control" }, "r", awesome.restart),
     awful.key({ modkey, "Shift"   }, "q", awesome.quit),
+    awful.key({ "Control", "Mod1" }, "Delete", function () awful.util.spawn("xscreensaver-command -lock") end),
 
     awful.key({ modkey,           }, "l",     function () awful.tag.incmwfact( 0.05)    end),
     awful.key({ modkey,           }, "h",     function () awful.tag.incmwfact(-0.05)    end),
