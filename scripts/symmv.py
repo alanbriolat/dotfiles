806e574d641ae979cf66daee35393cba3320517e
Alan Briolat <alan.briolat@gmail.com>
2012-10-22 23:01:44 +0100

    Add move-and-symlink script

---
+++ scripts/symmv.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python2
+"""Move files and symbolically link them back to their original locations.
+
+Depends on Python 2, the docopt module, and coreutils >= 8 (for the --relative
+option to ln).
+
+Usage:
+  symmv.py [options] SOURCE DEST
+  symmv.py [options] -t DIRECTORY SOURCE...
+  symmv.py -h | --help
+
+Options:
+  -d --dry-run      Only print commands, don't run them
+"""
+import os
+import subprocess
+from docopt import docopt
+
+
+def run(args, dryrun):
+    """Print and run *args*, unless *dryrun* is True."""
+    print(args)
+    if not dryrun:
+        subprocess.check_call(args)
+
+
+def move_single(source, dest, dryrun):
+    source = source.rstrip('/')
+    dest = dest.rstrip('/')
+    if os.path.isdir(dest):
+        dest = os.path.join(dest, os.path.basename(source))
+    if os.path.exists(dest):
+        raise Exception('destination "{}" already exists'.format(dest))
+    if os.path.islink(source):
+        raise Exception(('source "{}" is a symbolic link - moving it will '
+            'probably break it').format(source))
+
+    run(['mv', source, dest], dryrun)
+    run(['ln', '--symbolic', '--relative', dest, source], dryrun)
+
+
+def move_multiple(sources, dest, dryrun):
+    if not os.path.isdir(dest):
+        raise Exception('DIRECTORY must be a directory')
+    for source in sources:
+        move_single(source, dest, dryrun)
+
+
+if __name__ == '__main__':
+    args = docopt(__doc__)
+    print(args)
+    if args['-t']:
+        move_multiple(args['SOURCE'], args['DIRECTORY'], args['--dry-run'])
+    else:
+        move_single(args['SOURCE'][0], args['DEST'], args['--dry-run'])
