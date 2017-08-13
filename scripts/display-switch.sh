3658ab1177aeb1d0a675886d556f3cf6a4517e72
Alan Briolat <alan.briolat@hanzoarchives.com>
2017-08-13 22:10:27 +0100

    Update display-switch.sh for 4K monitor

--- scripts/display-switch.sh
+++ scripts/display-switch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-USAGE="usage: $0 (own|own_plus_dual|external_triple) [nvidia|intel]"
+USAGE="usage: $0 (own|own_plus_dual_home) [nvidia|intel]"
 LAYOUT="$1"
 DRIVER="$2"
 
@@ -20,14 +20,13 @@ case "$DRIVER" in
     nvidia)
         OUTPUT_OWN=eDP-1-1
         OUTPUT_LEFT=DP-1-1-2
-        OUTPUT_CENTRE=DP-1-2-1
-        OUTPUT_RIGHT=DP-1-2-2
+        OUTPUT_CENTRE=DP-1-2-2
+        #OUTPUT_RIGHT=DP-1-2-2
         ;;
     intel)
         OUTPUT_OWN=eDP-1
         OUTPUT_LEFT=DP-1-2
-        OUTPUT_CENTRE=DP-2-1
-        OUTPUT_RIGHT=DP-2-2
+        OUTPUT_CENTRE=DP-2-2
         ;;
     *)
         echo "$USAGE"
@@ -41,27 +40,16 @@ case "$LAYOUT" in
         --output $OUTPUT_OWN --auto --primary
         --output $OUTPUT_LEFT --off
         --output $OUTPUT_CENTRE --off
-        --output $OUTPUT_RIGHT --off
         "
         PANEL_ON=""     # "automatic"
         ;;
-    own_plus_dual)
+    own_plus_dual_home)
         XRANDR="
-        --output $OUTPUT_OWN --auto
-        --output $OUTPUT_LEFT --auto --primary --above $OUTPUT_OWN
-        --output $OUTPUT_CENTRE --auto --right-of $OUTPUT_LEFT
-        --output $OUTPUT_RIGHT --off
+        --output $OUTPUT_LEFT --auto --rotate left --pos 0x0
+        --output $OUTPUT_CENTRE --auto --primary --pos 1440x80
+        --output $OUTPUT_OWN --auto --pos 2400x2240
         "
-        PANEL_ON=""     # "automatic"
-        ;;
-    external_triple)
-        XRANDR="
-        --output $OUTPUT_OWN --off
-        --output $OUTPUT_LEFT --auto
-        --output $OUTPUT_CENTRE --auto --primary --right-of $OUTPUT_LEFT
-        --output $OUTPUT_RIGHT --auto --right-of $OUTPUT_CENTRE
-        "
-        PANEL_ON="monitor-1"
+        PANEL_ON="monitor-1"     # "automatic"
         ;;
     *)
         echo "$USAGE"
